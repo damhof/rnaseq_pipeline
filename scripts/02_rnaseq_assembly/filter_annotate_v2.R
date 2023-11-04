fd2d8e7b1ee56a0a152dd256e94d9934d3923520
Damon Hofman <d.a.hofman-3@prinsesmaximacentrum.nl>
2023-11-04 14:50:19 +0100

    fix(filter_annotate): Update gene and transcript filtering
    1. Remove genes with exons on both strands (usually small number of genes)
    2. Update same-strand class i overlap (haven't compared to original version)
    3. Update gene_id annotating step

--- scripts/02_rnaseq_assembly/filter_annotate_v2.R
+++ scripts/02_rnaseq_assembly/filter_annotate_v2.R
@@ -50,39 +50,27 @@ gtf_novel_GR <- rtracklayer::import(gtf_novel_file)
 
 ## Load original tracking file
 orig_tracking <- data.table::fread(orig_tracking_file, header = F, select = c(1:5)) %>%
-  tidyr::separate(V3, into = c("ref_gene_id_tracking", "ref_transcript_id"), sep = "\\|") %>%
+  tidyr::separate(V3, into = c("ref_gene_id_annotated", "ref_transcript_id_annotated"), sep = "\\|") %>%
   tidyr::separate(V5, into = c("query_gene_id", "query_transcript_id", "query_num_exons", "query_FPKM", "query_TPM", "query_cov", "query_len"), sep = "\\|")
-colnames(orig_tracking) <- c("TCONS", "xloc", "ref_gene_id_orig", "ref_transcript_id", "class_code_orig", "query_gene_id", "query_transcript_id", "query_num_exons", "query_FPKM", "query_TPM", "query_cov", "query_len")
+colnames(orig_tracking) <- c("TCONS", "xloc", "ref_gene_id_annotated", "ref_transcript_id_annotated", "class_code_annotated", "query_gene_id", "query_transcript_id", "query_num_exons", "query_FPKM", "query_TPM", "query_cov", "query_len")
 orig_tracking$query_gene_id <- sub("^q1:", "", orig_tracking$query_gene_id)
 
 ### Merge orig_tracking_file with annotated GTF (generated with gffcompare using stringtie --merge GTF as input and ref GTF as reference)
 orig_tracking_annot <- orig_tracking %>%
-  left_join(gtf_annot[, c("gene_name", "ref_gene_id", "cmp_ref", "cmp_ref_gene", "transcript_id")], by = c("query_transcript_id" = "transcript_id"))
-
-### Remove redundant lines from orig_tracking_annot
-orig_tracking_annot <- orig_tracking_annot %>%
-  group_by(TCONS) %>%
-  mutate(
-    gene_name = first(gene_name[gene_name != "NA"]),
-    ref_gene_id = first(ref_gene_id[ref_gene_id != "NA"]),
-    cmp_ref = first(cmp_ref[cmp_ref != "NA"]),
-    cmp_ref_gene = first(cmp_ref_gene[cmp_ref_gene != "NA"]),
-  ) %>%
-  ungroup() %>%
-  distinct()
+  left_join(subset(gtf_annot, type == "transcript")[, c("gene_name", "ref_gene_id", "cmp_ref", "cmp_ref_gene", "transcript_id")], by = c("query_transcript_id" = "transcript_id"))
 
 ## Load combined gffcompare tracking file
 combined_tracking <- data.table::fread(combined_tracking_file, header = F, select = c(1:5)) %>%
   tidyr::separate(V3, into = c("combined_gene_id", "ref_transcript_id"), sep = "\\|") %>%
   tidyr::separate(V5, into = c("query_gene_id", "query_transcript_id", "query_num_exons", "query_FPKM", "query_TPM", "query_cov", "query_len"), sep = "\\|")
-colnames(combined_tracking) <- c("TCONS", "xloc", "combined_gene_id", "combined_transcript_id", "class_code_combined", "query_gene_id", "query_transcript_id", "query_num_exons", "query_FPKM", "query_TPM", "query_cov", "query_len")
+colnames(combined_tracking) <- c("TCONS_combined", "xloc", "combined_gene_id", "combined_transcript_id", "class_code_combined", "query_gene_id", "query_transcript_id", "query_num_exons", "query_FPKM", "query_TPM", "query_cov", "query_len")
 
 ## Merge combined_tracking file with orig_tracking file to annotate combined GTF
-combined_tracking_annotated <- combined_tracking[, c("TCONS", "combined_gene_id", "combined_transcript_id", "class_code_combined")] %>%
-  left_join(orig_tracking_annot[, c("ref_gene_id", "ref_transcript_id", "query_gene_id", "query_transcript_id", "class_code_orig", "gene_name", "cmp_ref", "cmp_ref_gene")], na_matches = "never", by = c("combined_transcript_id" = "query_transcript_id"))
+combined_tracking_annotated <- combined_tracking[, c("TCONS_combined", "combined_gene_id", "combined_transcript_id", "class_code_combined")] %>%
+  left_join(orig_tracking_annot[, c("ref_gene_id_annotated", "ref_transcript_id_annotated", "query_gene_id", "query_transcript_id", "class_code_annotated", "gene_name", "cmp_ref", "cmp_ref_gene")], na_matches = "never", by = c("combined_transcript_id" = "query_transcript_id"))
 
 ## Load reference GTF
-gtf_reference <- rtracklayer::import.gff("/hpc/pmc_vanheesch/shared_resources/GENOMES/Homo_sapiens.GRCh38/102/annotation/Homo_sapiens.GRCh38.102.gtf", colnames = c(
+gtf_reference <- rtracklayer::import.gff(gtf_ref_loc, colnames = c(
   "type",
   "source",
   "gene_id",
@@ -104,6 +92,7 @@ gtf_novel_GR <- gtf_novel_GR[strand(gtf_novel_GR) != "*", ]
 gtf_novel <- data.table::as.data.table(gtf_novel_GR)
 gtf_novel$num_samples <- as.numeric(gtf_novel$num_samples)
 
+## Remove low abundance transcripts
 message(paste0(Sys.time(), "\t", "Filtering transcripts found in fewer than ", min_occurrence, " samples ..."))
 transcripts_keep <- subset(gtf_novel, type == "transcript" & num_samples >= min_occurrence)$transcript_id
 gtf_novel <- gtf_novel[which(gtf_novel$transcript_id %in% transcripts_keep),]
@@ -115,24 +104,57 @@ gtf_novel <- gtf_novel %>%
     gene_name = first(gene_name[gene_name != "NA"]),
     cmp_ref = first(cmp_ref[cmp_ref != "NA"]),
     cmp_ref_gene = first(cmp_ref_gene[cmp_ref_gene != "NA"]),
-    oId = first(oId[oId != "NA"])
+    # oId = first(oId[oId != "NA"])
   ) %>%
   ungroup()
 
 ## Merge gtf_novel with combined tracking files
 gtf_novel <- gtf_novel %>%
-  dplyr::left_join(combined_tracking_annotated, suffix = c(".gtf", ".annot"), by = c("transcript_id" = "TCONS"))
+  dplyr::left_join(combined_tracking_annotated, suffix = c(".gtf", ".annot"), by = c("transcript_id" = "TCONS_combined"))
 
 ## Remove unwanted transcript classes
 transcripts_discard <- gtf_novel[which(gtf_novel$type == "transcript" &
-                                                 gtf_novel$class_code_orig %in% c("=", "c", "j", "m", "n", "e", "r", "s")
+                                                 gtf_novel$class_code_annotated %in% c("=", "c", "j", "m", "n", "e", "r", "s")
   ), ]
-gtf_novel <-
-  gtf_novel[which(!(
-    gtf_novel$transcript_id %in% transcripts_discard$transcript_id
-  )), ]
+gtf_novel <- gtf_novel[which(!(gtf_novel$transcript_id %in% transcripts_discard$transcript_id)), ]
 
-# Set unique transcript IDs for each TCONS ID (gffcompare finds different TCONSs within same annotated parent transcript isoforms, resulting in multiple different isoforms with same MSTRG.X.X ID if not fixed)
+## Remove transcripts annotated as 'x-class STRG of x-class MSTRG'
+TCONS_discard <- subset(combined_tracking_annotated, class_code_combined == "x" & class_code_annotated == "x")$TCONS_combined
+gtf_novel <- gtf_novel[which(!(gtf_novel$transcript_id %in% TCONS_discard)), ]
+
+## Remove x and i transcripts with same-strand reference overlap (x should be opposite strand, same-strand i is not interesting)
+## Also sets gene_id
+### Reduce gtf_reference to unique gene_id - strand pairs
+gtf_reference_unique <- gtf_ref_df %>%
+  select(gene_id, strand) %>%
+  distinct()
+### Join the gtf_novel with the unique gtf_reference on ref_gene_id_annotated
+gtf_novel_joined <- left_join(gtf_novel, gtf_reference_unique, by = c("ref_gene_id_annotated" = "gene_id"))
+### Filter for x and i transcripts that have same strand as reference
+gtf_novel <- gtf_novel_joined %>%
+  group_by(transcript_id) %>%
+  filter(!is.na(ref_gene_id_annotated)) %>%
+  # Remove x and i transcripts that have same strand as reference
+  filter(
+    is.na(strand.y) |
+    (class_code_annotated %in% c('x', 'i') & strand.x != strand.y) |
+    (!class_code_annotated %in% c('x', 'i') & strand.x == strand.y)
+  ) %>%
+  mutate(
+    # Determine the gene_id based on ref_gene_id_annotated and strand
+    gene_id = case_when(
+      # If ref strand is NA, (i.e. no ref known) keep the combined_gene_id
+      is.na(strand.y) ~ combined_gene_id,
+      # If class_code_annotated is x or i and strand is different, keep the combined_gene_id
+      class_code_annotated %in% c('x', 'i') & strand.x != strand.y ~ combined_gene_id,
+      # If class_code_annotated is not x or i, strand is the same as reference, and ref_gene_annotated is known, keep the ref_gene_id_annotated
+      !class_code_annotated %in% c('x', 'i') & strand.x == strand.y & ref_gene_id_annotated != "-" ~ ref_gene_id_annotated,
+      # Otherwise, keep the combined_gene_id
+      TRUE ~ combined_gene_id
+    )
+  )
+
+## Set unique transcript IDs for each TCONS ID (gffcompare finds different TCONSs within same annotated parent transcript isoforms, resulting in multiple different isoforms with same MSTRG.X.X ID if not fixed)
 setDT(gtf_novel)
 setorder(gtf_novel, combined_gene_id, transcript_id)
 gtf_novel[, counter := cumsum(!duplicated(transcript_id)), by = combined_gene_id]
@@ -143,22 +165,25 @@ gtf_novel[, counter := NULL]
 gtf_novel <- gtf_novel %>%
   group_by(transcript_id) %>%
   mutate(
-    ref_gene_id = ref_gene_id,
-    gene_id = ifelse(!is.na(ref_gene_id), ref_gene_id, combined_gene_id),
-    ref_ranscript_id = ref_transcript_id,
-    transcript_id = combined_transcript_id,
-    gene_name = ifelse(!is.na(gene_name.annot), gene_name.annot, gene_name.gtf),
+    ref_gene_id = ref_gene_id_annotated,
+    # gene_id = ifelse(!is.na(ref_gene_id), ref_gene_id, combined_gene_id),
+    ref_ranscript_id = ref_transcript_id_annotated,
+    transcript_id = transcript_id_new,
+    gene_name = ifelse(!is.na(gene_name.annot), 
+                       gene_name.annot,
+                       ifelse(!is.na(gene_name.gtf), gene_name.gtf, cmp_ref_gene.gtf)),
     class_code = first(class_code[class_code != "NA"]),
-    class_code_orig = first(class_code_orig[class_code_orig != "NA"]),
+    class_code_annotated = first(class_code_annotated[class_code_annotated != "NA"]),
     # gene_name = ifelse(is.na(gene_name), gene_id, gene_name)
     ) %>%
   ungroup()
-gtf_novel <- subset(gtf_novel, class_code != "u")  # Need to find out what to do with these guys, as some of them occur in quite a lot of samples
+
+# gtf_novel <- subset(gtf_novel, gene_id != "-")  # Need to find out what to do with these guys, as some of them occur in quite a lot of samples
 
 ## Select relevant columns
 gtf_novel <- gtf_novel %>%
-  mutate(class_code = class_code_orig) %>%
-  dplyr::select(c(seqnames, start, end, width, strand, source, type, score, phase, gene_id, gene_name, transcript_id, ref_gene_id, class_code, tss_id, num_samples, exon_number, cmp_ref.annot, cmp_ref_gene.annot))
+  mutate(class_code = class_code_annotated) %>%
+  dplyr::select(c(seqnames, start, end, width, strand.x, source, type, score, phase, gene_id, gene_name, transcript_id, ref_gene_id, class_code, tss_id, num_samples, exon_number, cmp_ref.annot, cmp_ref_gene.annot))
 colnames(gtf_novel) <- c("seqnames", "start", "end", "width", "strand", "source", "type", "score", "phase", "gene_id", "gene_name", "transcript_id", "ref_gene_id", "class_code", "tss_id", "num_samples", "exon_number", "cmp_ref", "cmp_ref_gene")
 
 ## Flagging RefSeq transcripts
@@ -188,24 +213,42 @@ message(paste(Sys.time(), "Checking reference transcript overlap ..."),
 ref_overlap_txs <- suppressWarnings(check_tx_overlap(gtf = gtf_novel,
                                     gtf_reference = gtf_reference))
 
-## Annotate same-stranded i class
-message(paste(Sys.time(), "Remove same sense i class transcripts ..."),
-        sep = "\t")
-same_strand_i_txs <- suppressWarnings(filter_i_class(gtf_df = gtf_novel,
-                                    reference_granges = gtf_reference))
+# ## Annotate same-stranded i class
+# message(paste(Sys.time(), "Remove same sense i class transcripts ..."),
+#         sep = "\t")
+# same_strand_i_txs <- suppressWarnings(filter_i_class(gtf_df = gtf_novel,
+#                                     reference_granges = gtf_reference))
 
 ## Filter for final transcript subset
 gtf_novel <- subset(
-  gtf_novel,!(gtf_novel$transcript_id %in% same_strand_i_txs) &
-    !(gtf_novel$transcript_id %in% ref_overlap_txs)
+  gtf_novel, !(gtf_novel$transcript_id %in% ref_overlap_txs)
 )
 
+## Remove genes with exons on both strands
+# 1. Subset the GTF data to retain only exon entries
+exon_data <- gtf_novel[gtf_novel$type == "exon",]
+# 2. Group by gene and reference sequence, 
+# 3. Count unique strands for each gene-reference sequence combination
+gene_strand_count <- exon_data %>%
+  group_by(gene_id = exon_data$gene_id, seqnames) %>%
+  summarise(n_strands = n_distinct(strand)) %>%
+  ungroup()
+# 4. Identify genes with exons on both strands
+genes_both_strands <- gene_strand_count[gene_strand_count$n_strands > 1,]$gene_id
+# 5. Remove genes with exons on both strands
+gtf_novel <- subset(
+  gtf_novel, !(gtf_novel$gene_id %in% genes_both_strands)
+)
+# 6. Print message with number of genes with exons on both strands
+message(paste(Sys.time(), "Number of genes with exons on both strands:",
+              length(genes_both_strands)), sep = "\t")
+              
 ## Rename new stringtie genes
-message(paste(Sys.time(), "Rename novel genes ..."), sep = "\t")
-gtf_novel_stringtie <-
-  rename_stringtie_transcripts(gtf_novel_df = gtf_novel)
+# message(paste(Sys.time(), "Rename novel genes ..."), sep = "\t")
+# gtf_novel_stringtie <-
+#   rename_stringtie_transcripts(gtf_novel_df = gtf_novel)
 gtf_novel_GR <-
-  GenomicRanges::makeGRangesFromDataFrame(gtf_novel_stringtie, keep.extra.columns = T)
+  GenomicRanges::makeGRangesFromDataFrame(gtf_novel, keep.extra.columns = T)
 
 # Merge novel GTF with hg38 GTF
 gtf_novel_merged <- c(gtf_reference, gtf_novel_GR)
